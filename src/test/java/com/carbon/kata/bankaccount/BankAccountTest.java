eff30f85bbd848bef7673dbc4d20140d9221cff7
Florian <mail.de.florian.kaiser@gmail.com>
2019-01-25 22:01:43 +0100

    First commit with empty test

---
+++ src/test/java/com/carbon/kata/bankaccount/BankAccountTest.java
@@ -0,0 +1,14 @@
+package com.carbon.kata.bankaccount;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
